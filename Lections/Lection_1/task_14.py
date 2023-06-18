a45bde3b62eb56b397176e610dd86210b97deb09
IlyaChistov <gaplis322@gmail.com>
2023-06-18 21:35:10 +0300

    Init commit

---
+++ Lections/Lection_1/task_14.py
@@ -0,0 +1,3 @@
+animals = ['cat', 'dog', 'wolf', 'rate', 'bragon']
+for i, animal in enumerate(animals, start=1):
+    print(i, animal)
---
+++ Lections/Lection_1/task_9.py
@@ -0,0 +1,10 @@
+num = float(input('Введите число: '))
+count = 0
+while count < num:
+    print(count)
+    count += 2
+
+num = 0
+num = num + 3
+num += 3
+num //= 2
