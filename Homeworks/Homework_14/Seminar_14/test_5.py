b79881aa93f54ca02e90be521b247366595cc35a
IlyaChistov <gaplis322@gmail.com>
2023-08-11 18:09:41 +0300

    Homework 14

---
+++ Homeworks/Homework_14/Seminar_14/test_5.py
@@ -0,0 +1,26 @@
+# На семинарах по ООП был создан класс прямоугольник хранящий длину и ширину,
+# а также вычисляющую периметр, площадь и позволяющий складывать и вычитать прямоугольники беря за основу периметр.
+# Напишите 3-7 тестов unittest для данного класса.
+import unittest
+
+import pytest
+
+from task_5 import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    def setUp(self):
+        self.rectangle = Rectangle(3, 5)
+
+    def test_per(self):
+        self.assertEqual(Rectangle(3, 5).per(), 16)
+
+    def test_square(self):
+        self.assertEqual(Rectangle(3, 5).square(), 15)
+
+    def test_equal(self):
+        self.assertEqual(Rectangle(3, 5), self.rectangle)
+
+
+if __name__ == "__main__":
+    pytest.main(["-v"])
